e8091a2534928db9489167bc4417bef3d989bc28
kinyaelgrande <kinya.francis.mumbi@gmail.com>
2022-06-12 11:44:42 +0300

    Add template functionality and both inventory and warehouse templates

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
 
-var tpl = template.Must(template.ParseFiles("index.html"))
+type inventory struct{}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tpl.Execute(w, nil)
+	data := inventory{}
+	generateHTML(w, data, "layout", "navbar", "index")
+}
+
+func createInventory(w http.ResponseWriter, r *http.Request){
+	data := inventory{}
+	generateHTML(w, data, "layout", "navbar", "create.inventory")
+}
+
+func viewInventory(w http.ResponseWriter, r *http.Request){
+	data := inventory{}
+	generateHTML(w, data, "layout", "navbar", "view.inventory")
+}
+
+func editInventory(w http.ResponseWriter, r *http.Request){
+	data := inventory{}
+	generateHTML(w, data, "layout", "navbar", "edit.inventory")
+}
+
+//Warehouses
+func warehouses(w http.ResponseWriter, r *http.Request){
+	data := inventory{}
+	generateHTML(w, data, "layout", "navbar", "warehouses")
+}
+
+func createWarehouse(w http.ResponseWriter, r *http.Request){
+	data := inventory{}
+	generateHTML(w, data, "layout", "navbar", "create.warehouse")
 }
 
 func main() {
@@ -16,8 +42,18 @@ func main() {
 
 	//Serve static files
 	fs := http.FileServer(http.Dir("assets"))
-	
-	mux.HandleFunc("/", indexHandler)
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/", indexHandler)
+
+
+	mux.HandleFunc("/inventory/new", createInventory)
+	mux.HandleFunc("/inventory/view", viewInventory)
+	mux.HandleFunc("/inventory/edit", editInventory)
+
+	//warehouses
+	mux.HandleFunc("/warehouses", warehouses)
+	mux.HandleFunc("/create/warehouse", createWarehouse)
+
 	http.ListenAndServe(":8000", mux)
 }
